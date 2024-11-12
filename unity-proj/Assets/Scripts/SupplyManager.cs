5e23aeaa7ae188d9740e98b00aee72619be7dc81
jongwoo <izowooi@hotmail.com>
2024-11-12 23:31:33 +0900

    SuppyManager 임시로 채워넣음

--- unity-proj/Assets/Scripts/SupplyManager.cs
+++ unity-proj/Assets/Scripts/SupplyManager.cs
@@ -15,29 +15,52 @@ public class SupplyManager
             return instance;
         }
     }
+    
+    private int food;
+    private int currency;
+
+    private int price;
+    
+    public void Initizlie()
+    {
+        food = 100;
+        currency = 100;
+        price = 1;
+    }
 
     public void PurchaseFood()
     {
-        throw new System.NotImplementedException();
+        if( currency < price)
+        {
+            return;
+        }
+        food += 1;
+        currency -= price;
     }
 
     public string GetCurrentFood()
     {
-        throw new System.NotImplementedException();
+        return food.ToString();
     }
 
     public string GetCurrentCurrency()
     {
-        throw new System.NotImplementedException();
+        return currency.ToString();
     }
 
     public string GetFoodDetails()
     {
-        throw new System.NotImplementedException();
+        string ret = "식량 배급 : 2 \n" +
+                     $"농부가 0 식량을 생산했습니다.\n";
+
+        return ret;
     }
 
     public string GetCurrencyDetails()
     {
-        throw new System.NotImplementedException();
+        string ret = "작업자가 2 원 벌었습니다.\n" +
+                     $"기술자가 0 원 벌었습니다.\n";
+
+        return ret;
     }
 }
