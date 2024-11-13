845c7daddac6bbf6dba340d73263c137d9db069a
jongwoo <izowooi@hotmail.com>
2024-11-13 21:52:35 +0900

    게임 오버 UI 최초 추가

---
+++ unity-proj/Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverUI : MonoBehaviour
+{
+    [SerializeField] private Button restartButton;
+    private UIManager uiManager;
+    
+    private void Start()
+    {
+        uiManager = transform.parent.GetComponent<UIManager>();
+        // 버튼 이벤트 연결
+        restartButton.onClick.AddListener(OnRestartButton);
+    }
+
+    private void OnRestartButton()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
